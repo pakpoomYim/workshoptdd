fd6535c190bd75d764346d8653b8fa5e15fa6887
PAKPOOM <pakpoom@set.or.th>
2016-07-10 15:51:00 +0700

    Change Unit test 2

--- src/test/java/AuthenticationServiceTest.java
+++ src/test/java/AuthenticationServiceTest.java
@@ -13,7 +13,7 @@ public class AuthenticationServiceTest {
 	@Test public void 
 	username_not_existed() {
 		AuthenticationService authenticationService = new AuthenticationService(new NullUserDao());
-		assertTrue(authenticationService.isUsernameExisted("SOMKIAT"));
+		assertFalse(authenticationService.isUsernameExisted("SOMKIAT"));
 	}
 	
 }
